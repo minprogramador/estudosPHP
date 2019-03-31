086f67426689891410c6ef71c62aae08b633babd
minprogramador <minprogramador@gmail.com>
2019-03-31 20:33:15 -0300

    funcao lambda

---
+++ funcaoLambda.php
@@ -0,0 +1,12 @@
+<?php
+
+// # Funcao lambda.
+//
+// > É uma função anônima básica.
+//	Confira a sintaxe.
+
+$var = function() {
+	//code
+};
+
+// > Lambdas são mais rápidas que funçōes criadas utilizando create_function.
